c514f0c2745e780951148283dccb30ce15a24311
Joonas Tuomi <joonastapanituomi@gmail.com>
2026-02-23 10:32:50 +0200

    Renamed Fprint to Enroll

--- src/app/fprint.rs
+++ src/app/fprint.rs
@@ -23,7 +23,8 @@ pub async fn list_enrolled_fingers_dbus(
     username: String,
 ) -> zbus::Result<Vec<String>> {
     validate_username(&username)?;
-    device.list_enrolled_fingers(&username).await
+    let res = device.list_enrolled_fingers(&username).await
+
 }
 
 pub async fn delete_fingerprint_dbus(
