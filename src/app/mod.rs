5f2b2a64ceec89c1694e0e3c5071e4e76d220bd7
jtuomi <jtuomi@student.hive.fi>
2026-02-25 17:27:26 +0200

    Renamed icon.svg to enroll.svg so changed references to it

--- src/app/mod.rs
+++ src/app/mod.rs
@@ -32,7 +32,7 @@ use fprint::{
 use error::AppError;
 
 const REPOSITORY: &str = env!("CARGO_PKG_REPOSITORY");
-const APP_ICON: &[u8] = include_bytes!("../../resources/icons/hicolor/scalable/apps/icon.svg");
+const APP_ICON: &[u8] = include_bytes!("../../resources/icons/hicolor/scalable/apps/enroll.svg");
 const FPRINT_ICON: &[u8] = include_bytes!("../../resources/icons/hicolor/scalable/apps/fprint.svg");
 
 const STATUS_TEXT_SIZE: u16 = 16;
