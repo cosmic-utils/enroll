5483d0dc6c3e80f3143d793b23ec759dc706040c
Joonas Tuomi <git@joonastuomi.fi>
2026-09-21 17:38:20 +0300

    chore: cargo fmt

--- src/app/error.rs
+++ src/app/error.rs
@@ -149,8 +149,7 @@ mod tests {
     #[test]
     fn test_non_method_error() {
         // Test a different zbus::Error variant
-        let zbus_err =
-            zbus::Error::from(std::io::Error::other("test error"));
+        let zbus_err = zbus::Error::from(std::io::Error::other("test error"));
         let app_err = AppError::from(zbus_err);
 
         if let AppError::Unknown(msg) = app_err {
